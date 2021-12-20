bc2bcd49b19de15a662fc1befffb5c83214f446a
tresorsawasawa <tresorsawasawa@gmail.com>
2021-12-21 01:35:28 +0200

    Add add a team array members data

---
+++ js/team.js
@@ -0,0 +1,11 @@
+const teamMemberData = [
+  {
+    id: 1,
+    names: 'Tresor Sawasawa',
+    profilePicture: './images/profile1.JPG',
+    about: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo adipisci officia mollitia voluptatibus provident praesentium.',
+    work: 'Bill Gates now focuses most of his time on philanthropy, running the Bill & Melinda Gates Foundation alongside his wife.',
+  },
+];
+
+

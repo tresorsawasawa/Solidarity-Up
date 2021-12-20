251d0d417cd6b129b56e9f2f8f7f002206859e5b
tresorsawasawa <tresorsawasawa@gmail.com>
2021-12-21 01:36:51 +0200

    Add the team card and data in team.js file

--- js/team.js
+++ js/team.js
@@ -8,4 +8,17 @@ const teamMemberData = [
   },
 ];
 
-
+teamMemberData.forEach((member) => {
+  const card = document.createElement('li');
+  card.className = 'members-item';
+  card.innerHTML = `<div class="card-img">
+                      <img class="profile-img" src=${member.profilePicture} alt="Profile Pic"> 
+                      <img class="bg-img" src="./images/bg-grid.jpg" alt="Background-grid Pic"> 
+                    </div>
+                    <div class="card-info">
+                      <h2>${member.names}
+                      <h3>${member.about}
+                      <p>${member.work}></p>
+                    </div>
+                  `;
+});
